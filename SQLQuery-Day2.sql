8ee17be90ad0ec2d7839be798c788f76fc3f1ddf
Ayushree-2147141 <ayushree.chakrabartty@mca.christuniversity.in>
2023-09-25 10:15:46 +0530

    functions

--- SQLQuery-Day2.sql
+++ SQLQuery-Day2.sql
@@ -16,4 +16,34 @@ insert into student(firstName, lastName, className) values('Hrishik', 'Kumar', 1
 insert into student(firstName, lastName, className) values('Renil', 'Justin', 3);
 insert into student(firstName, lastName, className) values('Akash', 'Singh', 1);
 insert into student(firstName, lastName, className) values('Reuben', 'Kurian', 3);
+
 select * from student;
+
+-- system function / user defined functions
+-- cast for type casting
+
+--CONCATENATION
+select concat(firstName,' ',lastName) as 'Full Name'
+from student;
+
+select concat(firstName,' ',lastName) 'Full Name'
+from student;
+
+-- Functions 
+-- Mydb/programmability/functions/scalar-valued functions/<dbname>.<functionName>
+create function getFullName(@id int)
+returns varchar(200)
+as
+begin 
+return (select concat(firstName,' ',lastName) 'Full Name' from student where studentId = @id);
+end
+
+
+-- function call
+-- returns all value
+-- using 'from student' since student table contains studentId, and not passing value therefore need to refer
+select dbo.getFullName(studentId) as FullName from student;
+
+-- returns one particular
+-- no need to use 'from student' since direct value is being passed
+select dbo.getFullName(2) as 'Full Name' 
